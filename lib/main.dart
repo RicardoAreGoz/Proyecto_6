ac505b4b2ee62f8bd440186b908bd2c1bc3b2ee9
RicardoAreGoz <ricardoaregoz@outlook.com>
2024-10-15 13:09:31 -0700

    Agrega el url y la anonkey de la base de datos en supabase

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,9 @@ import 'login.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: '',
-    anonKey: '',
+    url: 'https://swtfclileswocqlkridr.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3dGZjbGlsZXN3b2NxbGtyaWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg0OTE4MzIsImV4cCI6MjA0NDA2NzgzMn0.57l_SskUVP27-FHPsz4FXbTRn7lOfRb_gt7lOB4Qs5I',
   );
   runApp(MainApp());
 }
