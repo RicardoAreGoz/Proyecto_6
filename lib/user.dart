58ce09f0917efe25e60903913926fc19d38355c3
RicardoAreGoz <ricardoaregoz@outlook.com>
2024-10-27 21:22:26 -0600

    update del user

--- lib/user.dart
+++ lib/user.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'dart:async';
 import 'login.dart';
 
 class UserPage extends StatefulWidget {
@@ -15,21 +16,52 @@ class _UserPageState extends State<UserPage> {
   final TextEditingController _searchController = TextEditingController();
   List<dynamic> _movies = [];
   String _searchQuery = '';
+  bool _isLoading = true;
+  Timer? _debounce;
 
   @override
   void initState() {
     super.initState();
     _fetchMovies();
+    _searchController.addListener(_onSearchChanged);
+  }
+
+  @override
+  void dispose() {
+    _debounce?.cancel();
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _onSearchChanged() {
+    if (_debounce?.isActive ?? false) _debounce?.cancel();
+    _debounce = Timer(const Duration(milliseconds: 500), () {
+      setState(() {
+        _searchQuery = _searchController.text;
+        _fetchMovies();
+      });
+    });
   }
 
   Future<void> _fetchMovies() async {
-    final response = await widget._supabase
-        .from('movies')
-        .select()
-        .ilike('title', '%$_searchQuery%');
     setState(() {
-      _movies = response;
+      _isLoading = true;
     });
+    try {
+      final response = await widget._supabase
+          .from('movies')
+          .select()
+          .ilike('title', '%$_searchQuery%');
+      setState(() {
+        _movies = response;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      print("Error: $e");
+    }
   }
 
   Future<void> _recommendMovie(int movieId) async {
@@ -77,30 +109,27 @@ class _UserPageState extends State<UserPage> {
                 hintText: 'Buscar películas...',
                 suffixIcon: Icon(Icons.search),
               ),
-              onChanged: (query) {
-                setState(() {
-                  _searchQuery = query;
-                });
-                _fetchMovies();
-              },
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: _movies.length,
-              itemBuilder: (context, index) {
-                final movie = _movies[index];
-                return ListTile(
-                  title: Text(movie['title']),
-                  subtitle: Text('Recomendaciones: ${movie['recommendations']}'),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.thumb_up),
-                    onPressed: () => _recommendMovie(movie['id']),
+            child: _isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : ListView.builder(
+                    itemCount: _movies.length,
+                    itemBuilder: (context, index) {
+                      final movie = _movies[index];
+                      return ListTile(
+                        title: Text(movie['title']),
+                        subtitle:
+                            Text('Recomendaciones: ${movie['recommendations']}'),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.thumb_up),
+                          onPressed: () => _recommendMovie(movie['id']),
+                        ),
+                        onTap: () => _showCommentsDialog(movie['id']),
+                      );
+                    },
                   ),
-                  onTap: () => _showCommentsDialog(movie['id']),
-                );
-              },
-            ),
           ),
         ],
       ),
@@ -137,8 +166,4 @@ class _UserPageState extends State<UserPage> {
       },
     );
   }
-}
-
-extension on Supabase {
-  rpc(String s, {required Map<String, int> params}) {}
 }
