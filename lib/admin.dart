8adfffb521b7e8303b2e1f5c7bbf26be3d9f34bd
RicardoAreGoz <ricardoaregoz@outlook.com>
2024-10-27 21:03:17 -0600

    update del admin

--- lib/admin.dart
+++ lib/admin.dart
@@ -1,52 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'login.dart';
+import 'addEditProperty.dart';
 
-class AdministratorPage extends StatelessWidget {
+class AdminPage extends StatelessWidget {
   final SupabaseClient _supabase = Supabase.instance.client;
 
-   AdministratorPage({super.key});
+  AdminPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Página de Administrador'),
+        title: const Text('Página de Admin'),
         actions: [
           IconButton(
             icon: const Icon(Icons.logout),
             onPressed: () async {
-              // Cerrar sesión dentro del onPressed
               await _supabase.auth.signOut();
-              // Redirigir al login
               Navigator.pushReplacement(
                 context,
-                MaterialPageRoute(builder: (context) => LoginPage()),
+                MaterialPageRoute(builder: (context) => const LoginPage()),
               );
             },
           ),
         ],
       ),
-      body: FutureBuilder(
-        future: _supabase
-            .from('profiles')
-            .select('full_name')
-            .eq('id', _supabase.auth.currentUser!.id)
-            .single(),
+      body: StreamBuilder<List<Map<String, dynamic>>>(
+        stream: _supabase.from('peliculas').stream(primaryKey: ['id']),
         builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
-          if (!snapshot.hasData) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
-          final userData = snapshot.data as Map<String, dynamic>;
-          return Center(
-            child: Text('Bienvenido, ${userData['full_name']}',
-                style: const TextStyle(fontSize: 24)),
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          final rawdata = snapshot.data!;
+          return ListView.builder(
+            itemCount: rawdata.length,
+            itemBuilder: (context, index) {
+              final data = rawdata[index];
+              return ListTile(
+                title: Text(data['titulo']),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Año de publicacion: ${data['año']}'),
+                    Text('Actores principales: ${data['actores']}'),
+                    Text('Categoria: ${data['categoria']}'),
+                    Text('Idioma: ${data['idioma']}'),
+                  ],
+                ),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AddEditMoviePage(movieData: data),
+                          ),
+                        );
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        _eliminarPropiedad(data['id'], context);
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddEditMoviePage(),
+            ),
           );
         },
       ),
     );
+  }
+
+  Future<void> _eliminarPropiedad(int id, BuildContext context) async {
+    final response = await _supabase.from('peliculas').delete().eq('id', id);
+
+    if (response.error == null) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Eliminada exitosamente'),
+      ));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error al eliminar: ${response.error!.message}'),
+      ));
+    }
   }
 }
