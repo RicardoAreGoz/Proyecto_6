888bcbcbec4b57681bd22a5d75755cfca51fb879
RicardoAreGoz <ricardoaregoz@outlook.com>
2024-10-27 18:44:44 -0600

    agrega el codigo de la api al main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<List<dynamic>> fetchMovies() async {
+  final response = await http.get(Uri.parse('http://127.0.0.1:5000/peliculas'));
+
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body);
+  } else {
+    throw Exception('Failed to load movies');
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
